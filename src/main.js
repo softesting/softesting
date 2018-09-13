ecaf04ea7574b664908e1e48e04ab6dd2b1ccee3
softesting <biuro.softesting@gmail.com>
2018-09-13 12:31:42 +0200

    vuentify

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(Vuetify)
